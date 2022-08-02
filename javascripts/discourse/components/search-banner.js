a95514380f8496908bc680d124681a7cdd20689c
Yoshiyuki Nagai <ynagai@my.bcit.ca>
2022-08-02 12:22:56 -0700

    optimize JS code

--- javascripts/discourse/components/search-banner.js
+++ javascripts/discourse/components/search-banner.js
@@ -1,7 +1,7 @@
 import Component from "@ember/component";
 import { inject as service } from "@ember/service";
 import { defaultHomepage } from "discourse/lib/utilities";
-import { and } from "@ember/object/computed";
+import { and, bool } from "@ember/object/computed";
 import discourseComputed, { observes } from "discourse-common/utils/decorators";
 
 export default Component.extend({
@@ -43,7 +43,7 @@ export default Component.extend({
     return currentRouteName.includes("categories");
   },
 
-  shouldDisplay: bool("displayForRoute"),
+  shouldDisplay: and("displayForRoute", "displayForUser"),
   onCategories: bool("isOnCategories"),
   // Setting a class on <html> from a component is not great
   // but we need it for backwards compatibility
