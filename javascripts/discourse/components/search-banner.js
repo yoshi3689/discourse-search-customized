bbabd429c3517a54ea0ba60a7ca2c2565104f4dd
Yoshiyuki Nagai <ynagai@my.bcit.ca>
2022-08-02 10:17:57 -0700

    make the q button dissapearon a single topic page

--- javascripts/discourse/components/search-banner.js
+++ javascripts/discourse/components/search-banner.js
@@ -43,7 +43,7 @@ export default Component.extend({
   },
 
   shouldDisplay: and("displayForUser", "displayForRoute"),
-  shouldQuestionBtnAppear: "isRouteForBtn",
+  shouldQuestionBtnAppear: and("isRouteForBtn", true),
   // Setting a class on <html> from a component is not great
   // but we need it for backwards compatibility
   @observes("shouldDisplay")
